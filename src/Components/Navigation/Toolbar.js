eb149bee2845b81c6a2f28f4b79d964768c314a9
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2019-11-10 23:57:16 -0500

    addition of toolbar and sidedrawer links (#8)

--- src/Components/Navigation/Toolbar.js
+++ src/Components/Navigation/Toolbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './toolbar.css'
 import DrawerToggleButton from "./SideDrawer/DrawerToggleButton";
+import Resume from "../../Assets/Resume-Anneliese.pdf";
 
 const  Toolbar  = props => (
     <header className="toolbar">
@@ -9,9 +10,18 @@ const  Toolbar  = props => (
             <div><DrawerToggleButton click={props.drawerClickHandler} /></div>
                 <div className="toolbar-nav-items">
                     <ul>
+                        <li>
+                            <a href={Resume}>resume</a>
+                        </li>
                             <li>
                                 <a href="/#project-component">projects</a>
                             </li>
+                        <li>
+                            <a href="/#about">about</a>
+                        </li>
+                        <li>
+                            <a href="/#contact">contact</a>
+                        </li>
                         </ul>
                     </div>
             </nav>
