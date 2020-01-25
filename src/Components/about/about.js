fbf5d57949ec5dd74cdf356515877d5a7c3a736b
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2020-01-25 11:17:07 -0500

    About section (#15)
    *  reformat layer about section + transitions
    
    * updated packages

--- src/Components/about/about.js
+++ src/Components/about/about.js
@@ -13,22 +13,20 @@ function About(props) {
             <img className="about-cloud" src={aboutCloud}/>
             <div className="about-container">
                 <div className="content">
-                <div id="about">
-                    <h2>about</h2>
-                    <p>
-                        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                    </p>
-                </div>
-                <div>
-                    <div id="contact">
-                        <h2 className="about-heading contact-h">contact</h2>
-                        <div className="contact-links">
-                            <a  className="icon" href="https://www.linkedin.com/in/anneliesew/"><img src={linkedinIcon}/>linkedin.com/in/anneliesew</a>
-                            <a className="icon" href="https://github.com/AnnelieseW"><img src={githubIcon}/> github.com/AnnelieseW</a>
-                            <a className="icon" href="https://www.linkedin.com/in/anneliesewen/"><img src={mailIcon}/>anniefangyiwen@gmail.com</a>
+                    <div id="about">
+                        <h2>about</h2>
+                        <p>
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        </p>
+                    </div>
+                        <div id="contact">
+                            <h2 className="about-heading contact-h">contact</h2>
+                            <div className="contact-links">
+                                <a  className="icon" href="https://www.linkedin.com/in/anneliesew/"><img src={linkedinIcon}/>linkedin.com/in/anneliesew</a>
+                                <a className="icon" href="https://github.com/AnnelieseW"><img src={githubIcon}/> github.com/AnnelieseW</a>
+                                <a className="icon" href="https://www.linkedin.com/in/anneliesewen/"><img src={mailIcon}/>anniefangyiwen@gmail.com</a>
                         </div>
                     </div>
-                </div>
                 </div>
             </div>
         </div>
