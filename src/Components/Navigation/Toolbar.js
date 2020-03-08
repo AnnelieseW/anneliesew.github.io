1dfb16c62987360d2545351f505e7ffc944fc64e
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2020-03-07 23:07:01 -0500

    about me update (#16)
    * about me update
    
    * add spacing btwn navigation links
    
    * spelling mistake

--- src/Components/Navigation/Toolbar.js
+++ src/Components/Navigation/Toolbar.js
@@ -9,14 +9,14 @@ const  Toolbar  = props => (
             <div id="toggle-button"><DrawerToggleButton click={props.drawerClickHandler} /></div>
                 <div className="toolbar-nav-items">
                     <ul>
-                            <li>
-                                <a href="/#project-component">work</a>
-                            </li>
+                        <li>
+                            <a href="/#project-component">work</a>
+                        </li>
                         <li>
                             <a href="/#about">about</a>
                         </li>
                         <li>
-                            <a href="/#contact">conect</a>
+                            <a href="/#contact">connect</a>
                         </li>
                         </ul>
                     </div>
--- src/Components/about/about.js
+++ src/Components/about/about.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AboutImg from "../../Assets/about-img.png";
+import Me from "../../Assets/Me.png";
 import aboutCloud from "../../Assets/about-cloud.svg";
 import linkedinIcon from "../../Assets/icon-linkedin.svg";
 import githubIcon from "../../Assets/icon-github.svg";
@@ -14,18 +15,33 @@ function About(props) {
             <div className="about-container">
                 <div className="content">
                     <div id="about">
-                        <h2>about</h2>
+                        <h2>Hi! 👋 </h2>
+                        <h2 id="name-h">I'm Anneliese</h2>
                         <p>
-                            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                            I'm a student in the School of Planning at the University of Waterloo. I am currently pursuing Honours Planning, specializing in land development, and a Joint Honours in Mathematics with a Computing Minor.
+                        </p>
+                        <p>
+                            I hope to combine both these interests and work at the intersection of Planning, Technology, and Transportation. I believe in the use of technology as a meaningful tool to inform planning decisions and am excited to see the integration of ubiquitous computing in our built environment.
+                           </p>
+                        <p>
+                            I'm an advocate for inclusive and accessible technology. You can find me running around organizing hackathons with StarterHacks (Canada’s largest beginner hackathon!) and EnergyHacks (bringing technology and policy together!), tinkering with code, or planning my next adventure.
+                        </p>
+                        <p>
+                            I'm currently seeking Fall 2020 internship opportunities in software development, product management and planning.
                         </p>
                     </div>
+
                         <div id="contact">
-                            <h2 className="about-heading contact-h">contact</h2>
+                            <img id="profile-pic" src={Me}/>
+                            <div className="contact">
+                            <h2 className="about-heading contact-h">let's chat</h2>
                             <div className="contact-links">
                                 <a  className="icon" href="https://www.linkedin.com/in/anneliesew/"><img src={linkedinIcon}/>linkedin.com/in/anneliesew</a>
                                 <a className="icon" href="https://github.com/AnnelieseW"><img src={githubIcon}/> github.com/AnnelieseW</a>
                                 <a className="icon" href="https://www.linkedin.com/in/anneliesewen/"><img src={mailIcon}/>anniefangyiwen@gmail.com</a>
+                            </div>
                         </div>
+
                     </div>
                 </div>
             </div>
