55292a29681bbeee1bc7f26e9bc8e8e8f8f2b384
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2020-04-11 18:27:58 -0400

    context switching (#30)

--- src/Components/Landing/LandingPage.js
+++ src/Components/Landing/LandingPage.js
@@ -3,27 +3,20 @@ import "./LandingPage.css";
 import Resume from '../../Assets/Resume-Anneliese.pdf';
 import "../../App.css"
 import Contact from "../contact/Contact";
+import Welcome from "./FunFont";
+import ResumeLink from "../ResumeLink/ResumeLink";
 
 
 function Landing(props) {
     return (
-        <div id="landing" className="sticky">
         <div className="landing">
             <div id="landing-block">
-            <h2>Anneliese Wen</h2>
-                <h6>q: why do you code if you study urban planning?</h6>
-                <h5>a: i wanted to make cool and useful things</h5>
-                <div >
-                <h6>looking for fall 2020 internship opportunities in software development or product management</h6>
-                </div>
-                <a id="resume-link" href={Resume}>
-                    <div>
-                       resume
-                    </div>
-                </a>
+            <Welcome/>
+                <div className="landing-p"><p>seeking fall 2020 internship opportunities in software development or product management</p>
+                <ResumeLink/>
                 <Contact/>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,54 @@
 import React, {Component} from 'react';
 import DefaultLanding from "./Default/defaultLanding";
+import Toggle from "./Components/Toggle/Toggle";
+import MainTech from "./MainTech";
+import Navigation from "./Components/Navigation/Navigation";
+import ThemeContext from "./ThemeContext";
+import Landing from "./Components/Landing/LandingPage";
 
 
 class Main extends Component {
+    constructor(props) {
+        super(props);
 
-    render() {
-        return (
-            <div id="main">
-                <DefaultLanding/>
-            </div>
-        )
+        this.toggleTheme = () => {
+            this.setState(state => ({
+                theme:
+                    state.theme === 'tech' ? 'planning' : 'tech',
+            }));
+            console.log(this.state)
+        };
+
+        this.state = {
+            theme: 'tech',
+            toggleTheme: this.toggleTheme,
+        };
     }
+    render() {
+        let landing;
+        let content;
+        if (this.state.theme === 'tech') {
+            content = <MainTech/>;
+            landing= <Landing/>;
+        } else {
+            content = <MainTech/>;
+            landing = <DefaultLanding/>;
+        }
+
+            return (
+                <div id="main">
+                    <ThemeContext.Provider value={this.state}>
+                    {/*<Navigation changeTheme ={this.toggleTheme}/>*/}
+                    <div id="landing" className={this.state.theme ==='tech'? 'landing-tech sticky': 'sticky'}>
+                        {landing}
+                        <Toggle/>
+                    </div>
+                    {/*{content}*/}
+                    </ThemeContext.Provider>
+                </div>
+            )
+        }
 }
 
+Main.contextType = ThemeContext;
 export default Main
