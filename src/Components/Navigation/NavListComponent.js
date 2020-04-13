832de69d547a8f782ce62ee43ff3d09b8c71f9f1
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2020-04-13 02:38:10 -0400

    hopeful

--- src/Components/Navigation/NavListComponent.js
+++ src/Components/Navigation/NavListComponent.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import AnchorLink from "react-anchor-link-smooth-scroll/src";
 
 class NavListComponent extends React.Component {
 
     render() {
         return(
             <li>
-                <Link to={this.props.item.href} onClick={this.props.click}>{this.props.item.text}</Link>
+                <AnchorLink to={this.props.item.href} onClick={this.props.click}>{this.props.item.text}</AnchorLink>
             </li>
         )
     }
