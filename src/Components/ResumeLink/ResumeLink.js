792e8971ade8cc685bc3daa0402afd07842f25cc
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2020-04-14 03:53:37 -0400

    switch context for resume

--- src/Components/ResumeLink/ResumeLink.js
+++ src/Components/ResumeLink/ResumeLink.js
@@ -1,15 +1,21 @@
 import React from "react";
-import Resume from '../../Assets/Resume-Anneliese.pdf'
+import Resume from '../../Assets/AnnelieseW-Resume.pdf'
+import PlanningResume from '../../Assets/AnnelieseW-planning.pdf'
 import './ResumeLink.css'
+import ThemeContext from "../../ThemeContext";
 
 class ResumeLink extends React.Component{
     render(){
         return(
+            <ThemeContext.Consumer>
+                {({theme}) => (
             <button id='resume-link'>
-                <a href={Resume}  target="_blank">
+                <a href={theme === 'tech'? Resume : PlanningResume}  target="_blank">
                     <h5>resume</h5>
                 </a>
             </button>
+                )}
+            </ThemeContext.Consumer>
         )
     }
 }
