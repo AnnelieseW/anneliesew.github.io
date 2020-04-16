35f65c60552f0df05f0da7868d965f3287cd1a00
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2020-04-15 21:14:35 -0400

    update description

--- src/Tech/TechProjectPages/ProjectList.js
+++ src/Tech/TechProjectPages/ProjectList.js
@@ -18,10 +18,12 @@ export const pomodoro = {
         tools: 'react.js, figma',
         learning: 'first taste of React, learned about passing props and states',
         timeline: "weekend in Oct '19",
-        status: "incomplete",
+        status: "V2 in progress",
         description: "Pomodoro is a study technique consisting of 25 minute intervals of intense focus followed by 5 minutes of rest. " +
             "While there are plenty of mobile apps available, I couldn't find a webapp that has all the functionality I was looking for." +
-            " I wanted an app  that switches between study and break time automatically as well as keep count of how many cycles have passed, so I made my own! ",
+            " I wanted an app  that switches between study and break time automatically as well as keep count of how many cycles have passed, so I made my own! " +
+            "I am currently in the progress of rewriting the web-app as I look back and realize my code is hideous, the new version is not ready yet. " +
+            "You're able to take a look at the original in the meantime.",
         links: {
             site:'https://anneliesew.github.io/pomodoro/',
             github: 'https://github.com/AnnelieseW/pomodoro',
